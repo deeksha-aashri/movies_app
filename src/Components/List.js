fc0fd03eb9352e350da4e00067e9e25466de9252
deeksha-aashri <deekshaaashri1@gmail.com>
2022-07-16 20:25:04 +0530

    :memo: Data from database extracted using axios

--- src/Components/List.js
+++ src/Components/List.js
@@ -1,10 +1,15 @@
 import React, { Component } from "react";
-import { movies } from "./getMovies";
+// import { movies } from "./getMovies";
+import axios from "axios";
+import API_KEY from '../secrets'
 export default class List extends Component {
   constructor(){
     super();
     this.state={
       hover:"",
+      page_arr:[1],
+      currPage:1,
+      movies:[]
     };
   }
 
@@ -19,20 +24,38 @@ export default class List extends Component {
       hover:""
     })
   };
-render(){
   
- let movie=movies.results;
+ async componentDidMount(){
+  // console.log("checking");
+  // console.log(API_KEY);
+  let db_result=await axios.get(
+    `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en-US&page=${this.state.currPage}`
+  );
+  // let data=db_result.data;
+  // console.log(data);
+  this.setState({
+    movies:[...db_result.data.results]//.data to convert it into readable form and .result because result brings an object and we just need the key called results of it
+  })
+ }
+
+
+render(){
+  console.log("render working");
+//  let movie=movies.results;
  return(
   <>
   <h2 className="text-center">Trending</h2>
-  {movie.length ===0 ? 
-  <div class="spinner-border text-info" role="status">
-  <span class="visually-hidden">Loading...</span>
+  {this.state.movies.length ==0 ? (
+  <div class="spinner-border text-info" role="status" >
+  <span class="visually-hidden" style={{display:"flex" , justifyContent:"center"}}>Loading...</span>
    </div>
-  :
-  <div className="movies-list">
-    {movie.map((movieObj)=>
-     <div className="movie-card"
+   ) :  
+
+    (
+    <div className="movies-list">
+    {this.state.movies.map((movieObj)=> (
+     <div 
+     className="movie-card"
      onMouseEnter={()=>{this.handleMouseEntering(movieObj.id)}}
      onMouseLeave={this.handleMouseLeaving}
      >
@@ -43,19 +66,45 @@ render(){
      ></img>
      <h5 className="movie-title card-title">{movieObj.original_title}</h5>
      <div className="button-wrapper">
-        {this.state.hover==movieObj.id &&   <button type="button" className="btn btn-info">Add to favorite</button>
-        }
+        {this.state.hover==movieObj.id &&  ( 
+        <a href="#"  className="btn btn-info" >
+          Add to favorite
+          </a>)}
      
        </div>
    </div>
+    )
     )}
-   
-    
+    <div className="pagination">
+   <nav aria-label="Page navigation example">
+  <ul className="pagination justify-content-center">
+    <li className="page-item disabled">
+      <a className="page-link">Previous</a>
+    </li>
+    <li className="page-item"><a className="page-link" href="#">1</a></li>
+    {/* <li class="page-item"><a class="page-link" href="#">2</a></li>
+    <li class="page-item"><a class="page-link" href="#">3</a></li> */}
+
+{
+                      this.state.page_arr.map(pageNum => (
+                        <li class="page-item">
+                          <a class="page-link" href="#">
+                            {pageNum}
+                          </a>
+                        </li>
+                      ))
+                    }
+    <li className="page-item">
+      <a className="page-link" href="#">Next</a>
+    </li>
+  </ul>
+</nav>
+</div>
   </div>
-}
+)}
    
   </>
- )
+ );
 }
 }
 
